e85f0c7312cd3414bc2b2a0aa2bfa29d95dd25d6
FYH620 <78263637+FYH620@users.noreply.github.com>
2022-02-27 17:26:30 +0800

    Update Draw_One_Image_With_BBox.py

--- Draw_One_Image_With_BBox.py
+++ Draw_One_Image_With_BBox.py
@@ -1,5 +1,4 @@
 import numpy as np
-from PIL import ImageDraw, Image,ImageFont
 import cv2
 
 class DrawOnePictureWithBBox(object):
@@ -9,26 +8,32 @@ class DrawOnePictureWithBBox(object):
         Args:
             index_to_labels(dict):{0:'areoplane',...}      index to labels
             img(ndarray):a numpy image                     opencv-image
-            bbox_coords(tensor/array):shape [num_bboxes,4] absolute coords
+            bbox_coords(tensor/array):shape [num_bboxes,4] absolute coords([xmin,ymin,xmax,ymax])
             bbox_labels(tensor/array):shape [num_bboxes]   number labels with each bbox
             bbox_scores(tensor):shape [num_bboxes]         scores with each bbox
         Return:
             An image with bounding boxes
         '''
 
-        self.img = Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+        self.img = img
         self.bbox_coords = bbox_coords
         self.bbox_labels = bbox_labels
         self.bbox_scores = bbox_scores
         self.index_to_labels = index_to_labels
 
     def __call__(self):
-        draw = ImageDraw.Draw(self.img)
-        font = ImageFont.truetype("msyh.ttc",20,encoding="utf-8")
+
         for i in range(len(self.bbox_coords)):
-            draw.rectangle(self.bbox_coords[i], outline='red')
+
+            min_xy=(self.bbox_coords[i,0],self.bbox_coords[i,1])
+            max_xy=(self.bbox_coords[i,2],self.bbox_coords[i,3])
+            cv2.rectangle(self.img,min_xy,max_xy,color=(255,0,0),thickness=3)
+
             texts = str(self.index_to_labels[self.bbox_labels[i]])
             if self.bbox_scores is not None:
                 texts += str(np.round(self.bbox_scores[i], decimals=2))
-            draw.text((self.bbox_coords[i][0], self.bbox_coords[i][1]),texts,fill='blue',font=font)
-        self.img.show()
+            cv2.putText(self.img,texts,min_xy,cv2.FONT_HERSHEY_PLAIN,color=(0,0,255),thickness=1,fontScale=2)
+
+        cv2.imshow('img-with-box:press q to exit',self.img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
